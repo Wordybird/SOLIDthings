2294acc5d7a4b85922352115ff9ec1420b876aea
Wordybird <ben.a.faulkner@btinternet.com>
2016-12-07 14:15:22 +0000

    Created test sheet for muggles.

---
+++ wizard_management_specs/MuggleTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import wizard_management.*;
+
+public class MuggleTest {
+
+  Muggle muggle;
+  Flyable plane;
+
+  @Before
+  public void before() {
+    plane = new Plane("Aerobus");
+    muggle = new Muggle("Toby", plane);
+  }
+
+  @Test
+  public void hasName() {
+    assertEquals("Toby", muggle.getName());
+  }
+
+  @Test
+  public void canSetRide() {
+    Helicopter helicopter = new Helicopter("Airwolf");
+    muggle.setRide(helicopter);
+    assertEquals("Spinning the propellors, generating speed, lift off!", muggle.fly());
+  }
+
+  @Test
+  public void hasPlane() {
+    Plane plane = (Plane) muggle.getFlyable();
+    assertEquals("Aerobus", plane.getName());
+  }
+
+  @Test
+  public void canFlyPlane() {
+    assertEquals("Revving the engines, driving down the runway, lift off!", muggle.fly());
+  }
+
+  @Test
+  public void canFlyHelicopter() {
+    Helicopter helicopter = new Helicopter ("Tilly");
+    Muggle muggle = new Muggle ("Thomas", helicopter);
+    assertEquals("Spinning the propellors, generating speed, lift off!", muggle.fly());
+  }
+
+}
